1b53a97094b1545d7fa45ecc12eef3c0c0619d5f
Tessakam <tessa.kam@hotmail.com>
2020-10-26 10:12:16 +0100

    initial commit

---
+++ connection.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+//connect with database "import.sql"
+function openConnection($dbuser, $dbpass): PDO
+{
+    $dbhost = "localhost";
+    $db = "price_calculator_database";
+    $driverOptions = [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+
+    return new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass, $driverOptions);
+}
+
