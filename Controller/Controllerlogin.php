b75a84587a59ad32a974b8e174d5df2c7fac9c88
Tessakam <tessa.kam@hotmail.com>
2020-10-28 17:08:43 +0100

    working on login page

--- Controller/Controllerlogin.php
+++ Controller/Controllerlogin.php
@@ -1,7 +1,39 @@
 <?php
-
+// Create an auth.php file and write both the login and registration logic in them
 
 class Controllerlogin
 {
+    public function render(array $GET, array $POST)
+    {
+        // Check if email is valid. Must contain a valid email address (with @ and .)
+        if (empty($_POST["email"])) {
+            $email = "Username is required";
+        } else {
+            $email = test_input($_POST["email"]);
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $email = "Invalid email format";
+            }
+        }
 
+        // Submit login + completed
+        if (isset($_POST['login']) && !empty($_POST['username'])
+            && !empty($_POST['password'])) {
+
+            if ($_POST['username'] == 'username@email.com' &&
+                $_POST['password'] == '1234') {
+                $_SESSION['valid'] = true;
+                $_SESSION['timeout'] = time();
+                $_SESSION['username'] = 'username@email.com';
+                $successmessage = 'Sign-in successful!';
+
+            } else {
+                $errormessage = 'Wrong username or password';
+            }
+
+            if (isset ($_SESSION['valid']) && $_SESSION['valid'] == true) ;
+
+
+            require 'View/register.php';
+        }
+    }
 }
--- View/register.php
+++ View/register.php
@@ -6,7 +6,7 @@ require 'includes/header.php';
 <section class="container">
     <form action="index.php" method="post">
         <br>
-        <H3>mySQL database: BECODE</H3>
+        <H3>mySQL database: BECODE registration</H3>
         <br>
 
         <?php if (isset($students)): ?>
@@ -22,15 +22,15 @@ require 'includes/header.php';
         </div>
         <div class="form-group">
             <label for="formGroupExampleInput2">First name</label>
-            <input type="text" class="form-control" name="firstname" id="firstname" placeholder="first name" required>
+            <input type="text" class="form-control" name="firstname" id="firstname" placeholder="first name" >
         </div>
         <div class="form-group">
             <label for="formGroupExampleInput2">Last name</label>
-            <input type="text" class="form-control" name="lastname" id="lastname" placeholder="last name" required>
+            <input type="text" class="form-control" name="lastname" id="lastname" placeholder="last name" >
         </div>
         <div class="form-group">
             <label for="formGroupExampleInput2">Email</label>
-            <input type="text" class="form-control" name="email" id="email" placeholder="email" required>
+            <input type="text" class="form-control" name="email" id="email" placeholder="email" >
         </div>
 
         <div>
@@ -39,27 +39,31 @@ require 'includes/header.php';
 
         <!-- Login page -->
         <br>
-        <H3>mySQL database: login</H3>
+        <H3>mySQL database: BECODE login</H3>
         <br>
 
         <form class = "form-signin " role = "form"
               action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);
               ?>" method = "post">
             <input type = "text" class = "form-control mr-sm-2"
-                   name = "username" placeholder = "username"
-                   required autofocus></br>
+                   name = "username" placeholder = "username"><br>
             <input type = "password" class = "form-control mr-sm-2"
-                   name = "password" placeholder = "password" required> <br>
+                   name = "password" placeholder = "password"><br>
             <button class = "btn btn-primary" type = "submit"
                     name = "login">Login</button>
         </form>
 
         <?php if (isset($errormessage)): ?>
-            <div class="alert alert-success" role="alert">
+            <div class="alert alert-warning" role="alert">
                 <?php echo $errormessage ?>
             </div>
         <?php endif; ?>
 
+        <?php if (isset($successmessage)): ?>
+            <div class="alert alert-success" role="alert">
+                <?php echo $successmessage ?>
+            </div>
+        <?php endif; ?>
 
     </form>
 
